3a83fa8a31578e873063fe4322b0b115b4cdeb4d
JamesWP <james_peach01@hotmail.co.uk>
2015-10-10 11:54:24 +0100

    added blank execution controller class

---
+++ api-server/SqlNotebookApiServer/Controllers/ExecutionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SqlNotebookApiServer.Controllers
+{
+    /// <summary>
+    /// The execution controller deals with creating a connection to a sql server 
+    /// instance and running queries and retreiving results
+    /// </summary>
+    public class ExecutionController : ApiController
+    {
+        
+    }
+}
