be5d4babe390b8a47c994e532fc8abf3ca58b535
JamesWP <james_peach01@hotmail.co.uk>
2015-10-11 21:50:04 +0100

    added execution creation and result parser

--- api-server/SqlNotebookApiServer/Controllers/ExecutionController.cs
+++ api-server/SqlNotebookApiServer/Controllers/ExecutionController.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace SqlNotebookApiServer.Controllers
@@ -13,6 +14,104 @@ namespace SqlNotebookApiServer.Controllers
     /// </summary>
     public class ExecutionController : ApiController
     {
-        
+        public ExecutionContextManager CM = new ExecutionContextManager();
+
+        [HttpPost]
+        public Guid CreateExecutionContext(string Database, string Server, string Username, string Password) {
+            return CM.New(Database, Server, Username, Password);
+        }
+
+        [HttpPost]
+        public async Task<JValue> Execute(Guid Context, string query)
+        {
+            var context = CM.Get(Context);
+            var result = await new SqlCommand(query, context).ExecuteReaderAsync();
+            var resultObj = new ExecutionResult(result);
+            var resultJson = JsonConvert.SerializeObjectAsync(resultObj);
+        }
     }
+
+    public class ExecutionContextManager
+    {
+        private Dictionary<Guid, SqlConnection> Contexts;
+
+        public Guid New(string Database, string Server, string Username, string Password)
+        {
+            var g = Guid.NewGuid();
+            var connectionString = new SqlConnectionStringBuilder() {
+                ApplicationName = "SqlNotebook",
+                InitialCatalog = Database,
+                Password = Password,
+                UserID = Username
+            };
+            connectionString["server"] = Server;
+            var context = new SqlConnection(connectionString.ConnectionString);
+            context.Open();
+            Contexts[g] = context;
+            return g;
+        }
+
+        public SqlConnection Get(Guid context)
+        {
+            return Contexts.ContainsKey(context) ? Contexts[context] : null;
+        }
+    }
+
+    public class ExecutionResult
+    {
+        public List<PartialResult> Parts = new List<PartialResult>();
+        public ExecutionResult(SqlDataReader result)
+        {
+            while (result.HasRows)
+            {
+                var Part = new PartialResult(result);
+                result.NextResult();
+                Parts.Add(Part);
+            }
+        }
+    }
+
+    public class PartialResult
+    {
+        public List<PartialColumn> Columns;
+        public List<PartialRow> Rows;
+        public PartialResult(SqlDataReader result)
+        {
+            ParseColumns(result);
+            while (result.Read())
+            {
+                var row = new PartialRow(result);
+            }
+        }
+
+        private void ParseColumns(SqlDataReader result)
+        {
+            for(var i = 0; i < result.FieldCount; i++)
+            {
+                var col = new PartialColumn(result, i);
+                Columns.Add(col);
+            }
+        }
+    }
+
+    public class PartialColumn
+    {
+        public string Type;
+        public string Name;
+        public PartialColumn(SqlDataReader result, int i)
+        {
+            Type = result.GetFieldType(i).ToString();
+            Name = result.GetName(i);
+        }
+    }
+
+    public class PartialRow
+    {
+        public object[] Values;
+        public PartialRow(SqlDataReader result)
+        {
+            result.GetValues(Values);
+        }
+    }
+
 }
